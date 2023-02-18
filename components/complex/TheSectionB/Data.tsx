b61d812ca19b59c4499fbcaacd1f4b344cdfb596
Marcin Zaczkowski <marcin.zaczkowski@sobre.pl>
2023-02-18 14:05:18 +0100

    Animated Count Logic

--- components/complex/TheSectionB/Data.tsx
+++ components/complex/TheSectionB/Data.tsx
@@ -4,29 +4,30 @@ import ActiveUser from "~/assets/svg/ActiveUser.svg";
 import Reviews from "~/assets/svg/Reviews.svg";
 
 import { ANIMATED_COUNT_LABELS } from "./Labels";
+import { IAnimatedCountItemProps } from "~~/components/parts/AnimatedCount/AnimatedCountItem.vue";
 
-export const ARRAY__ANIMATED_COUNT = [
+export const ARRAY__ANIMATED_COUNT: IAnimatedCountItemProps[] = [
   {
     icon: <Downloads />,
-    endCount: 25,
+    endAmount: 25,
     suffix: "K+",
     description: ANIMATED_COUNT_LABELS.ANIMATED_COUNT_FIRST_DESCRIPTION,
   },
   {
     icon: <Heart />,
-    endCount: 1200,
+    endAmount: 1200,
     suffix: "+",
     description: ANIMATED_COUNT_LABELS.ANIMATED_COUNT_SECOND_DESCRIPTION,
   },
   {
     icon: <ActiveUser />,
-    endCount: 14,
+    endAmount: 14,
     suffix: "K+",
     description: ANIMATED_COUNT_LABELS.ANIMATED_COUNT_THIRD_DESCRIPTION,
   },
   {
     icon: <Reviews />,
-    endCount: 3500,
+    endAmount: 3500,
     suffix: "+",
     description: ANIMATED_COUNT_LABELS.ANIMATED_COUNT_FOURTH_DESCRIPTION,
   },
--- components/parts/AnimatedCount/AnimatedCountItem.vue
+++ components/parts/AnimatedCount/AnimatedCountItem.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
 export interface IAnimatedCountItemProps {
   icon: Component;
-  endCount: number;
+  startAmount?: number;
+  endAmount: number;
+  duration?: number;
   suffix: string;
   description: string;
 }
 
-const { icon, endCount, suffix, description } = defineProps<IAnimatedCountItemProps>();
+const { icon, startAmount, endAmount, duration, suffix, description } = withDefaults(defineProps<IAnimatedCountItemProps>(), {
+  startAmount: 0,
+  duration: 2,
+});
 </script>
 
 <template>
-  <div class="flex flex-col items-center text-white"><component :is="icon"  class="mb-[25px]"/>
-    <p class="text-[22px] mb-[14px]">
-        {{endCount}}{{suffix}}
-    </p>
-    <p class="text-[10px]">{{description}}</p>
+  <div class="flex flex-col items-center text-white">
+    <component :is="icon" class="mb-[25px]" />
+    <p class="mb-[14px] text-[22px]"><vue3-autocounter :startAmount="startAmount" :endAmount="endAmount" :duration="duration" />{{ suffix }}</p>
+    <p class="text-[10px]">{{ description }}</p>
   </div>
 </template>
