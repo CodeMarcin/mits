0bc2dc46b6c079fb28c6250394d5952c2ada1ccc
Marcin Zaczkowski <marcin.zaczkowski@sobre.pl>
2023-02-20 12:08:13 +0100

    Create own Animate Count Fnc

--- components/complex/TheSectionB/TheSectionB.vue
+++ components/complex/TheSectionB/TheSectionB.vue
@@ -6,7 +6,7 @@ import { ARRAY__ANIMATED_COUNT } from "./Data";
   <div
     class="bg-blue flex skew-y-[2deg] px-6 py-[110px] before:absolute before:translate-y-1/2 before:left-0 before:content-[url('~/assets/png/LeftBackgroundSectionB.png')] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:content-[url('~/assets/png/RightBackgroundSectionB.png')]"
   >
-    <div class="container z-10 mx-auto flex w-full max-w-xl flex-col px-6 -skew-y-[2deg]">
+    <div class="container   z-10 mx-auto flex w-full max-w-xl flex-col px-6 -skew-y-[2deg]">
       <PartsAnimatedCount :props="ARRAY__ANIMATED_COUNT" />
     </div>
   </div>
--- components/complex/TheSectionC/Data.ts
+++ components/complex/TheSectionC/Data.ts
@@ -1,4 +1,5 @@
 import { IHeaderProps } from "~~/components/parts/Header/Header.vue";
+import { ICarouselProps } from "~~/components/parts/Carousel/Carousel.vue";
 import { HEADER_LABELS } from "./Labels";
 
 import Carousel1 from "~/assets/svg/Carousel1.svg";
@@ -7,12 +8,6 @@ import Carousel3 from "~/assets/svg/Carousel3.svg";
 import Carousel4 from "~/assets/svg/Carousel4.svg";
 import Carousel5 from "~/assets/svg/Carousel5.svg";
 
-// import Carousel1 from `~/assets/svg/Carousel1.svg`;
-// import Carousel2 from `~/assets/svg/Carousel2.svg`;
-// import Carousel3 from `~/assets/svg/Carousel3.svg`;
-// import Carousel4 from `~/assets/svg/Carousel4.svg`;
-// import Carousel5 from `~/assets/svg/Carousel5.svg`;
-import { ICarouselProps } from "~~/components/parts/Carousel/Carousel.vue";
 
 export const OBJECT__HEADER: IHeaderProps = {
   title: HEADER_LABELS.SECTION_C_SUBTITLE,
